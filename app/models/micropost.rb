749adb2485bb5eb9888ffd706d9e8f916d4ea4d9
kohei.baba <kbforanypurpose@gmail.com>
2019-03-02 03:51:37 +0000

    kadai-microposts ver1

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum:255 }
+  
+  has_many :likes
+  has_many :liked, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,20 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+
+  has_many :likes
+  has_many :ilikes, through: :likes, source: :micropost
+  
+  def like(micropost)
+    self.likes.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    like = self.likes.find_by(micropost_id: micropost.id)
+    like.destroy if like
+  end
+  
+  def ilike?(micropost)
+    self.ilikes.include?(micropost)
+  end
 end
